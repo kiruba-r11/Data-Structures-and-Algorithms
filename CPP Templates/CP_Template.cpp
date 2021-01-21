a31b38c8c5922ccdf2b411dd312d6c6f781700ee
Kirubanand <prdkiruba@gmail.com>
2021-01-21 14:51:17 +0530

    CP_Template added in CPP Folder

---
+++ CPP Templates/CP_Template.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                       long long
+#define ld                       long double
+#define m1                       1000000007
+#define m2                       998244353
+#define inf                      LLONG_MAX
+#define ninf                     LLONG_MIN
+#define v                        vector
+#define st                       stack
+#define pq                       priority_queue
+#define pb(i)                    push_back(i)
+#define pll(i)                   printf("%lld" , i)
+#define sll(i)                   scanf("%lld" , &i)
+#define sp(n , i)                fixed << setprecision(i) << n 
+#define debug(x , y)             cout << x << " " << y << endl
+#define endl                     "\n"
+
+ll solve() {
+
+	ll ans = 0;
+
+	// ll n;
+	// cin >> n;
+
+	// v<ll> a(n);
+	// for(ll i = 0; i < n; i++)
+		// cin >> a[i];
+
+	
+
+	return ans;
+
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt" , "r" , stdin);
+	freopen("output.txt" , "w" , stdout);
+#endif
+
+	ios_base::sync_with_stdio(false); 
+        cin.tie(NULL);
+
+	int tc = 1;
+	// cin >> tc;
+
+	// int x = 1;
+
+	while(tc--) {
+
+		// cout << "Case #" << x << " : ";
+		// x++;
+
+		ll ans = solve();
+		cout << ans << endl;
+	}
+
+	return 0;
+}
