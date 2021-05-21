ae6951d2b60b1fd84dc4c8c3c3c1d2865c9b8a83
Kirubanand <prdkiruba@gmail.com>
2021-05-21 22:37:20 +0530

    Doubly LL folder added

---
+++ Linked Lists/Doubly LL/insert_at_begin.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+class dll_node {
+public:
+	dll_node* prev;
+	int data;
+	dll_node* next;
+};
+
+void createDLL(dll_node* &head) {
+
+	int choice;
+
+	do {
+
+		int data;
+
+		cout << "Enter Data : ";
+		cin >> data;
+
+		dll_node* newNode = new dll_node();
+		newNode->data = data;
+		newNode->prev = NULL;
+		newNode->next = NULL;
+
+		if(head == NULL) {
+			head = newNode;
+		} else {
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+
+		cout << "Do you want to continue? (1/0) : ";
+		cin >> choice;
+
+	} while(choice == 1);
+
+
+}
+
+void display(dll_node* head) {
+	cout << "The elements are : ";
+	while(head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+int main() {
+
+	dll_node* head = NULL;
+
+	createDLL(head);
+	display(head);
+
+	return 0;
+}
